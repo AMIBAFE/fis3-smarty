73832254f715771319760a9bded23a71f471d261
pk <984504727@qq.com>
2017-06-17 10:09:58 +0800

    fis-config

--- fis-conf.js
+++ fis-conf.js
@@ -39,28 +39,36 @@ fis.media('debug').match('*.{js,css,png}',{
 },true)
 
 //某些资源不产出(后台发布使用)
-fis.media('go').match(/test|.*\.conf|plugin|.*\.md/,{
-  release:false
-})
-//产出目录
-  .match('{/site/(**.tpl),/widget/(**.tpl)}',{
-    release:'resources/views/project/$0'
-  })
-  .match('(**.js)',{
-    release:'public/js/$1'
-  })
-  .match('::image',{
-    release:'public/img/$0'
-  })
-  .match('**.{css,less,scss,sass}',{
-    release:'public/css/$0'
-  })
-  .match('**.{ttf,otf,woff,eot}',{
-    release:'public/font/$0'
-  })
-  .match('**.{mp4,avi,mkv,swf,wmv,txt,json,map}',{
-    release:'public/other/$0'    
-  })
-  .match(/project-map.json/,{
-	  release:'public/config/$0'
-  })
+fis.media('go').match(/test|.*\.conf|plugin|.*\.md/, {
+        release: false
+    })
+    //产出目录
+    .match('{/site/(**.tpl),/widget/(**.tpl)}', {
+        release: 'resources/views/project/$0'
+    })
+    .match('*.png', {
+        useHash: false
+    })
+    .match('(**.js)', {
+        release: 'public/js/$1',
+        url: '/js$1'
+    })
+    .match('::image', {
+        release: 'public/img/$0',
+        url: '/img$0'
+    })
+    .match('**.{css,less,scss,sass}', {
+        release: 'public/css/$0',
+        url: '/css$0'
+    })
+    .match('**.{ttf,otf,woff,eot}', {
+        release: 'public/font/$0',
+        url: '/font$0'
+    })
+    .match('**.{mp4,mp3,avi,mkv,swf,wmv,txt,json,map,wav}', {
+        release: 'public/other/$0',
+        url: '/other$0'
+    })
+    .match(/project-map.json/, {
+        release: 'public/config/$0'
+    })
